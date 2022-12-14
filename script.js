8a94739c82c23206457b055b066447bdbe8d31c6
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2022-12-14 19:58:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // updates the clock function
 function updateClock() {
-    var nd = new Date(); // choose the new date
-    var hour = nd.getHours(), // hour
-        minu = nd.getMinutes(), // minutes
-        seco = nd.getSeconds(), // seconds
-        tp = "AM"; // PM or AM
+    var nd = new Date();     // choose the new date
+    var hr = nd.getHours();  // hour
+    var minu = nd.getMinutes();  // minutes
+    var seco = nd.getSeconds();  // seconds
+    var tp = "AM";   // PM or AM
 
     // condition on hours, if they are 0 
-    if(hour == 0) {
-        hour = 12;
+    if(hr == 0) {
+        hr = 12;
     }
     // conditions on hours if they are greater than 12
-    if(hour > 12) {
-        hour = hour - 12;
-        tp = "PM";
+    // then subtract them from 12
+    if(hr > 12) {
+        hr = hr - 12;
+        tp = "PM"; // since after noon starts
     }
+    
     Number.prototype.pad = function(digits) {
-        for(var x = this.toString(); x.length < digits; x = 0 + x) {
-            return x;
+        for(var n = this.toString(); n.length < digits; n = 0 + n) {
+            return n;
         }
     }
-    var ids = ["hr", "min", "sec", "per"];
-    var values = [hour.pad(2), minu.pad(2), seco.pad(2), tp];
+    var ids = ["hour", "minute", "second", "period"];
+    var values = [hr.pad(2), minu.pad(2), seco.pad(2), tp];
     for(var i = 0; i < ids.length; i++) {
         document.getElementById(ids[i]).firstChild.nodeValue = values[i];
     }
@@ -33,3 +35,5 @@ function initClock() {
     window.setInterval("updateClock()", 1);
 }
 
+// Main function to run the code
+initClock();
